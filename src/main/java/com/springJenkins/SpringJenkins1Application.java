400d268b9bd89632d0680a9c0e663c38f6537d1a
PratikSalgar <161682344+PratikSalgar@users.noreply.github.com>
2024-11-09 19:16:07 +0530

    new commit added

--- src/main/java/com/springJenkins/SpringJenkins1Application.java
+++ src/main/java/com/springJenkins/SpringJenkins1Application.java
@@ -1,12 +1,19 @@
 package com.springJenkins;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringJenkins1Application {
 
+	public static Logger logger = LoggerFactory.getLogger(SpringJenkins1Application.class);
+	
 	public static void main(String[] args) {
+		
+		logger.info("Application Started Successfully.. ");
+		
 		SpringApplication.run(SpringJenkins1Application.class, args);
 	}
 
