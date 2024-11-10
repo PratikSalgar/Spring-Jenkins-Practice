9ad1bd507ef4fc8ec8827aa4ae2f9e1aa16fa6f7
PratikSalgar <161682344+PratikSalgar@users.noreply.github.com>
2024-11-10 11:49:47 +0530

    new commit

--- src/main/java/com/springJenkins/MyController.java
+++ src/main/java/com/springJenkins/MyController.java
@@ -1,12 +1,13 @@
 package com.springJenkins;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyController 
 {
-	@GetMapping("/hello")
+	@PostMapping("/hello")
 	public String getHello()
 	{
 		return "Hello";
